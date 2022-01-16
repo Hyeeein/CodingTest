7882c0eb0be2d4d517d531f55e1d0f76f94b0052
Hyeeein <phi498@naver.com>
2022-01-16 20:29:01 +0900

    update 0114

--- 2501_약수구하기.py
+++ 2501_약수구하기.py
@@ -17,3 +17,22 @@ if len(K_list) < K:
     print('0')
 else:
     print(K_list[K-1])
+
+## k_list = [i for i in range(1, N+1) if N % i == 0]
+
+## 정윤님 답안
+# try:
+#     n, k = map(int, input('숫자를 입력해 주세요').split())
+#     if not (1 <= n <= 10000 and 1 <= k <= n): # 예외 범위 정하기
+#         raise Exception('잘못 입력하셨습니다.')
+#     # a=[i for i in range(1, A+1) if A%i == 0]
+#     a = []
+#     for i in range(1, n + 1):
+#         if n % i == 0:
+#             a.append(i)
+#
+#     print(a[k - 1])
+# except IndexError:
+#     print(0)
+# except Exception as e:
+#     print(e)
--- 3460_이진수.py
+++ 3460_이진수.py
@@ -1,9 +1,23 @@
 # 이진수
 
-T = int(input())
+T = int(input())     # 테스트 개수
 for _ in range(T):
     n = bin(int(input()))[2:]
     for i in range(len(n)):
         if n[-i-1] == '1':
             print(i, end=' ')
     print()
+
+
+## 정윤님 답안
+#
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     i = 0
+#     while n > 0:
+#         if n % 2 == 1:
+#             print(i, end=' ')
+#         n //= 2
+#         i += 1
+#     print()
